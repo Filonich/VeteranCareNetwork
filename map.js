225dc72916b818c6a26cd2296a53cb080299d361
Filonich <www1.opp@gmail.com>
2017-01-02 11:46:12 +0200

    Initial commit

---
+++ map.js
@@ -0,0 +1,10 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 47.964, lng: 37.015},
+        zoom: 18,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+}
+function newMenu() {
+    
+}
